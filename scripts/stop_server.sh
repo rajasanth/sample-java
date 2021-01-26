61eaa0eb558c98ed4e2ea335627822b7ec54c363
rajasanth <42927089+rajasanth@users.noreply.github.com>
2021-01-27 01:51:20 +0530

    Update stop_server.sh

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,6 +1,6 @@
 
 #!/bin/bash
-isExistApp=`pgrep java`
-if [[ -n  $isExistApp ]]; then
-   service tomcat8 stop
-fi
+#isExistApp=`pgrep java`
+#if [[ -n  $isExistApp ]]; then
+ #  service tomcat8 stop
+#fi
